365c49d129ed61b98e146dacb70bec946c39014c
gom20 <rhaldud89@gmail.com>
2023-03-13 19:59:38 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:todo/screens/calendar.dart';
 import 'package:todo/screens/home.dart';
@@ -15,26 +15,19 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "MyApp",
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
       theme: ThemeData(
-        // Define the default brightness and colors.
         brightness: Brightness.light,
-        primaryColor: Colors.brown[800],
-
-        // Define the default font family.
         fontFamily: 'Pretendard',
-
-
-        // Define the default `TextTheme`. Use this to specify the default
-        // text styling for headlines, titles, bodies of text, and more.
-        textTheme: const TextTheme(
-          // displayLarge: TextStyle(fontFamily: 'Pretendard', fontSize: 72.0, fontWeight: FontWeight.w700),
-          titleLarge: TextStyle(fontFamily: 'Pretendard',fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.black),
-          bodyMedium: TextStyle(fontFamily: 'Pretendard',fontSize: 14.0, fontWeight: FontWeight.w700),
-        ),
+        // textTheme: const TextTheme(
+        //   titleLarge: TextStyle(fontFamily: 'Pretendard',fontSize: 20.0, fontWeight: FontWeight.w500, color: Colors.black),
+        //   bodyMedium: TextStyle(fontFamily: 'Pretendard',fontSize: 14.0, fontWeight: FontWeight.w700),
+        // ),
       ),
       initialRoute: '/home',
       routes: {
+        '/splash' : (context) => SplashScreen(),
         '/home' : (context) => HomeScreen(),
         '/calendar': (context) => CalendarScreen(),
       },
@@ -42,3 +35,61 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) => HomeScreen()
+            )
+        )
+    );
+  }
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        body: Container(
+            width: double.infinity,
+            color: Color(0xff689972),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin:EdgeInsets.fromLTRB(0, 0, 0, 2),
+                  child: Text(
+                    "오늘도 감사",
+                    style: TextStyle(
+                      color: Color(0xffFFF1DE),
+                      fontFamily: "NotoSerifKR",
+                      fontWeight: FontWeight.w500,
+                      fontSize: 40,
+                    ),
+                  ),
+                ),
+                Container(
+                  margin:EdgeInsets.fromLTRB(0, 0, 0, 10),
+                  child: Text(
+                    "Gratitude Journal",
+                    style: TextStyle(
+                        color: Color(0xffFFF1DE),
+                        fontWeight: FontWeight.w200,
+                        fontFamily: 'NotoSerifKR',
+                        fontSize: 15
+                    ),
+                  ),
+                )
+              ],
+            )
+        ),
+      );
+    }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -13,11 +13,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     int selectedDay = int.parse(DateFormat('yyyyMMdd').format(DateTime.now()));
 
-    final arguments = (ModalRoute.of(context)?.settings.arguments ?? <String, dynamic>{}) as Map;
+    final arguments = (ModalRoute
+        .of(context)
+        ?.settings
+        .arguments ?? <String, dynamic>{}) as Map;
     final textFieldController = TextEditingController();
 
     @override
@@ -26,71 +53,167 @@ class _HomeScreenState extends State<HomeScreen> {
       super.dispose();
     }
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("오늘도 감사해"),
-        titleTextStyle: TextStyle(
-          fontFamily: 'Pretendard',
-          color: Color(0xffE69E82),
-          fontWeight: FontWeight.w500,
-          fontSize: 20.0
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: AssetImage('assets/images/bg-image.png'),
         ),
-        backgroundColor: Colors.white10,
-        elevation: 0,
       ),
-      body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(20),
-          child:
-          Column(
-              children: [
-                Flexible(
-                  flex: 2,
-                  child: Text(
-                    DateFormat.yMMMd('ko_KR').format(DateTime.parse('$selectedDay')),
-                    style: TextStyle(
-                      fontFamily: 'Pretendard',
-                      fontWeight: FontWeight.w300,
-                      color: Color(0xffE69E82),
-                      fontSize: 30.0
-                    ),
-                  ),
-                ),
-                Flexible(
-                  flex: 8,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          controller: textFieldController,
-                          decoration: InputDecoration(
-                            labelText: 'Input',
-                          ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Container(
+          color: Colors.transparent,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Flexible(
+                    flex: 2,
+                    child: Container(
+                      child: Text(
+                        DateFormat('yyyy.MM.dd').format(
+                            DateTime.parse('$selectedDay')),
+                        style: TextStyle(
+                            fontFamily: 'NotoSerifKR',
+                            fontWeight: FontWeight.w600,
+                            color: Colors.black87,
+                            fontSize: 30.0
                         ),
                       ),
-                    ],
+                    )
                   ),
-                ),
-                Flexible(
-                  flex: 2,
-                  child: TextButton(
-                    onPressed: (){
-                      DBHelper dbHelper = DBHelper();
-                      dbHelper.insertGratitude(Gratitude(id: int.parse(DateFormat('yyyyMMdd').format(DateTime.now())), note: textFieldController.text));
-                      Navigator.pushNamed(context, '/calendar');
-                      // Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (BuildContext context){
-                      //   return CalendarScreen();
-                      // }), (r){
-                      //   return false;
-                      // });
-                    },
-                    child: Text("다음"),
+                  Flexible(
+                    flex: 8,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: double.infinity,
+                            padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                            child: Text("오늘의 감사함",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontFamily: 'NotoSerifKR',
+                                fontWeight: FontWeight.w500,
+                                fontSize: 13,
+                              ),
+                            )
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 4,
+                            cursorColor: Colors.brown,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontSize: 17,
+                                  color: Colors.white10
+                              ),
+                              hintText: '오늘도 감사한 하루였나요?',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                          child: Text("오늘의 다짐",
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'NotoSerifKR',
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13,
+                            ),
+                          )
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: Colors.white70,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: TextField(
+                            keyboardType: TextInputType.multiline,
+                            maxLines: 4,
+                            cursorColor: Colors.brown,
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.white10
+                              ),
+                              hintText: '오늘도 감사한 하루였나요?',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ]
+                  Flexible(
+                      flex: 2,
+                      child: Container(
+                        width: double.infinity,
+                        height: 40.0,
+                        margin: EdgeInsets.all(10),
+                        child: ElevatedButton(
+                          onPressed: () {
+                            debugPrint('ElevatedButton Clicked');
+                          },
+                          style: ElevatedButton.styleFrom(
+                            // shape: StadiumBorder(),
+                            elevation: 0,
+                            backgroundColor: Color(0xff689972),
+                          ),
+                          child: Text('기록 하기'),
+                        ),
+                      )
+                  ),
+                ]
+              ),
+            ),
           ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.edit_calendar),
+                label: "쓰기"
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: '기록',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              label: '설정',
+            ),
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Color(0xff689972),
+          selectedLabelStyle: TextStyle(
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w500,
+            fontSize: 11,
+          ),
+          unselectedLabelStyle: TextStyle(
+            fontFamily: 'Pretendard',
+            fontWeight: FontWeight.w400,
+            fontSize: 10,
+          ),
+          onTap: _onItemTapped,
         ),
       ),
     );
